025d23a9a715a83c65b5a0190973e7ae2631023d
dhilipkmr <dhilip1211@gmail.com>
2018-12-14 23:56:49 +0530

    form 1

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router';
 import Helmet from 'react-helmet';
+var session = require('express-session');
 
 import App from '../src/app'
 import mongoose from './db/mongoose';
@@ -15,10 +16,16 @@ import {usersModel} from './models/userModel';
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(session({
+    secret: 'dhilipLocal',
+    resave: false,
+    saveUninitialized: true
+  }))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('build/public'));
 app.use('/styles', express.static('src/pages/styles'));
+app.use(express.static('src/pages/static'));
 
 app.post('/signup', (request, response) => {
    const {username = '', password = '', emailId = ''} = request.body;
@@ -32,6 +39,8 @@ app.post('/signup', (request, response) => {
             response.send({error: true, msg: 'Username already Exists'});
         } else {
             user.save().then((doc) => {
+                request.session.user = doc.username;
+                console.log(' doc.username', doc.username);
                 response.send(doc);
             }, (e) => {
                 response.status(500).send(e);
@@ -47,7 +56,8 @@ app.post('/signin', (request, response) => {
     const {username = '', password = '', emailId = ''} = request.body;
      usersModel.find({username: username, password: password}).then((res)=> {
          if (res.length > 0) {
-             response.send({error: false, msg: 'success'});
+            request.session.user = username;
+            response.send({error: false, msg: 'success'});
          } else {
             response.send({error:true, msg: 'No user account found'});
          }
@@ -77,9 +87,11 @@ const loadHtml = (content) => {
                 ${helmet.meta.toString()}
                 ${helmet.title.toString()}
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-                <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js">
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js">
                 <link rel="stylesheet" type="text/css" href="/styles/common.css">
+                <link rel="stylesheet" type="text/css" href="/styles/home.css">
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
             </head>
             <body>
                 <div id="root">${content}</div>
--- src/pages/Components/Home.js
+++ src/pages/Components/Home.js
@@ -4,14 +4,67 @@ import Helmet from 'react-helmet';
 export default class Home extends Component {
   constructor(props) {
     super(props);
+    this.leftMenuClick = this.leftMenuClick.bind(this);
+    this.state = {
+      visibleLeftMenu: false,
+      visibleRightMenu: false
+    }
+  }
 
+  leftMenuClick() {
+    this.refs.backDrop.classList.toggle('backDrop');
+    this.refs.popup.classList.toggle('right0');
+    this.refs.firstHalfLandingTxt.classList.toggle('scale90');
+    this.refs.otherHalfLandingTxt.classList.toggle('scale90');
+  }
+
+  renderLeftMenuBar() {
+    return(
+      <div className="menuBar">
+        <div ref="backDrop" className="transition2a zi1 ">
+        </div>
+        <div ref="popup"className="popup zi2 " onClick={this.leftMenuClick}>
+          <div className="sideBar in-bl fl">
+            <div className="menu-option">Settings</div>
+            <div className="menu-option">Sign In</div>
+            <div className="menu-option">About Me</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
+    // const {visibleLeftMenu = false, visibleRightMenu = false} = this.state;
     return (
-      <div className="container-fluid">
-        <h1>Expense Home</h1>
-        <p></p> 
+      <div className="">
+        {this.renderLeftMenuBar()}
+        <div ref="mainContent" className="mainContent">
+          <div className="first-half-landing">
+            <div ref="firstHalfLandingTxt" className="firstHalfTxt">
+              <div className="standing">
+                <span className="left-menu-container" onClick={this.leftMenuClick}><img className="left-menu" src="/img/menu.svg"/></span>
+                <span></span>
+                {/* <span className="right-menu-container" onClick={() => {this.setState({visibleRightMenu: true})}}><img className="right-menu" src="/img/menu.svg"/></span> */}
+              </div>
+              <div className="heading">Expense Home</div>
+              <div className="expenseDaysBtn">
+                <span className="dayTypeBtn">Week</span>
+                <span className="dayTypeBtn">Month</span>
+                <span className="dayTypeBtn">Year</span>
+              </div>
+            </div>
+          </div>
+          <div className="other-half-landing">
+            <div ref="otherHalfLandingTxt">
+              <div className="newContainer">
+                <div className="new">
+                  <span className="dayTypeBtn">Add New</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
