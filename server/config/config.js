ff4490561e7cc296cb85a305870a7739e5c6e278
Dhilip kumar Velusamy <dhilip1211@gmail.com>
2018-12-23 12:31:35 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  // process.env.PORT = 4000;
-  // process.env.MONGOLAB_URI = 'mongodb://localhost:27017/expense';
-} else {
+// if (env === 'development') {
+//   // process.env.PORT = 4000;
+//   // process.env.MONGOLAB_URI = 'mongodb://localhost:27017/expense';
+// } else {
   process.env.MONGOLAB_URI = 'mongodb://dhilipk13:dhilipk13@ds247310.mlab.com:47310/expense';
-}
-export default env;
+// }
+// export default env;
