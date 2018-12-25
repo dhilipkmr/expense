09e9adae68cac54d572feeba3fd190f844e2f7ad
dhilipkmr <dhilip1211@gmail.com>
2018-12-25 20:47:35 +0530

    plot text

--- src/pages/Components/Graph.js
+++ src/pages/Components/Graph.js
@@ -1,9 +1,9 @@
 
-const WIDTH = 150;
-const HEIGHT = 100;
+const WIDTH = 180;
+const HEIGHT = 115;
 
 import React, {Component} from 'react';
-import {DIVISIONLENGTH} from '../constants/constants';
+import {DIVISIONLENGTH, MAXLENGTHPERTAB, MONTHSNAMESHORT, MONTH, WEEK, YEAR, WEEKNAMESHORT} from '../constants/constants';
 
 export default class Graph extends Component {
   constructor(props) {
@@ -19,14 +19,44 @@ export default class Graph extends Component {
     }
   }
 
+  renderPoints(xCoordinates, yCoordinates) {
+    const pointsElement = [];
+    const tab = this.props.tab;
+    let textIndex = 0;
+    for(let index = 1; index < xCoordinates.length - 1; index++) {
+      let pointText = '';
+      if (tab === WEEK) {
+        pointText = WEEKNAMESHORT[textIndex++].toUpperCase();
+      } else if (tab === YEAR) {
+        pointText = MONTHSNAMESHORT[textIndex++].toUpperCase();
+      } else if (tab === MONTH ) {
+        if (index % 2 !== 0) {
+          continue;
+        }
+        pointText = index;
+      }
+      pointsElement.push(
+        <g key={'group_' + index}>
+          <a>
+          <text className="fb" x={xCoordinates[index]} fill="#757575" y={yCoordinates[index] - 5} fontSize="4" textAnchor="middle">{pointText}</text>
+          <circle cx={xCoordinates[index]} cy={yCoordinates[index]} stroke="#4688F1" fill="#4688F1" r="1" strokeWidth="1"></circle>
+          </a>
+        </g>
+      );
+    }
+    return pointsElement;
+  }
+
   generateSVG() {
     const {plotData, tab} = this.props;
     const xCoordinates = [];
     const yCoordinates = [];
     const length = DIVISIONLENGTH[tab];
+    const maxLeng = MAXLENGTHPERTAB[tab];
     const maxAmt = plotData.maxAmount;
-    const xCoordinateDivLength = (WIDTH / (length + 2));
-    let lastDivision = -1;
+    const xCoordinateDivLength = (WIDTH / length);
+    let lastDivision = tab === MONTH ? 0 : -1; // Because we show only dates in month division begins with 1 so last shud be 0 
+
     let str = '';
 
     /* To start the graph at the Least Point */
@@ -46,7 +76,7 @@ export default class Graph extends Component {
       }
     });
     // to push values for remaining days
-    while(length > lastDivision) {
+    while(maxLeng > lastDivision) {
       const lastX = xCoordinates[xCoordinates.length - 1];
       xCoordinates.push((lastX + xCoordinateDivLength));
       yCoordinates.push(HEIGHT);
@@ -62,11 +92,9 @@ export default class Graph extends Component {
     if (str) {
       return (
         <div>
-          <svg viewBox={'0 0 ' + WIDTH + ' ' + HEIGHT} style={{margin: '20px'}}>
+          <svg viewBox={'-5 -10 ' + (WIDTH + 35) + ' ' + (HEIGHT + 15)} style={{margin: '10px'}}>
             <polyline points={str} className="graphPlotLine" />
-            {/* <g style={{stroke: '#ccc', strokeDasharray: 0, strokeWidth: 1}}>
-              <line x1="0" y1="200" x2="0" y2="0"></line>
-            </g> */}
+            {this.renderPoints(xCoordinates, yCoordinates)}
           </svg>
         </div>
       );
